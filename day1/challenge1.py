318ba8a5e1dff7b662c8d61b93bc33b8fe23dc8b
Chris Kapend <chris@lenomad.dev>
2024-03-30 17:36:58 -0400

    day 1

---
+++ day1/challenge1.py
@@ -0,0 +1,16 @@
+a = input("a: ")
+b = input("b: ")
+
+# Printing before swap
+print("\na = " + a)
+print("b = " + b + "\n")
+
+# Code swapping
+c = a
+a = b
+b = c
+
+# Printing after swap
+print("a = " + a)
+print("b = " + b)
+# trying to see the changes
---
+++ day1/challenge2.py
@@ -0,0 +1,5 @@
+print("Bonjour et Bienvenue to the band name generator")
+city = input("What city did you grew up in?\n")
+pet_name = input("What is the name of your pet\n")
+band_name = city + " " + pet_name
+print("Your band name is: " + band_name)
