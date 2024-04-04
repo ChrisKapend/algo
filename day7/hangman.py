86bb5f1e7a2a6fdfa698dd36cbdc26c4c2d315f5
Chris Kapend <chris@lenomad.dev>
2024-04-04 12:48:10 -0400

    Day 7 - Hangman game last part

--- day7/hangman.py
+++ day7/hangman.py
@@ -14,17 +14,27 @@ for i in range(word_length):
 # TODO - 2 Ask the user to guess a letter and assign their answer to a variable called guess. Make Guess lowercase
 while "_" in display and lives > 0:
     guess = str(input("Guess a letter: "))[0].lower()
+    # Clearing the screen
+    clear = lambda: os.system('cls')
+    clear()
     # TODO - 3 Check if the letter the user guessed (guess) is one of the letters in the chosen_word
+    if guess in display:
+        # Give user feedback on what he guessed
+        print(f"You've already guessed the letter '{guess}'.")
     if guess not in chosen_word.lower():
+        # Give user feedback on his guess
+        print(f"You've guessed the word '{guess}'. but it is not in the word")
         lives -= 1
         print(stages[lives])
     for i in range(word_length):
         letter = chosen_word[i]
         if letter.lower() == guess:
             display[i] = letter
+    # Print the word instead of the array containing the letters
     print(f"{' '.join(display)}")
 else:
     if "_" not in display:
         print(f"You win")
     else:
+        print(f"The word was: {chosen_word}")
         print(f"Game Over")
